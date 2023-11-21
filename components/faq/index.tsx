ac54cb00b75cfb9c218503a610345ab55f66dc7d
sayjun0505 <sayjun0505@gmail.com>
2023-11-21 11:45:14 -0800

    feat

--- components/faq/index.tsx
+++ components/faq/index.tsx
@@ -1,41 +1,29 @@
 "use client"
-import {Select, SelectItem} from "@nextui-org/react";
+import {Accordion, AccordionItem} from "@nextui-org/react";
   
 export default function Faq() {   
+    const defaultContent =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
        return (
         <div className="mx-auto text-white border-2 border-red-500 w-[1180px] h-[824px]">
             <div className="flex flex-col">
                 <div className="h-[200px] text-5xl border-2 border-red-500 flex justify-center items-center">Frequently asked questions</div>
-                <div className="border-2 border-red-500 pt-4 pl-4 mx-36">  
-                <Select 
-                    label="What is Viralyst?" 
-                    placeholder="Select an animal"
-                    disabledKeys={["zebra", "tiger", "lion", "elephant", "crocodile", "whale"]}
-                    className="max-w-xs"
-                >
-                    <SelectItem key="first question" value="this">
-                        Viralyst is an innovative committee initiated by an experienced expertise to consult a means of earning through advertisement and monetizing companies brand 
-                    </SelectItem>
-                    <SelectItem key="first question" value="this">
-                        AAAAAA
-                    </SelectItem>
-                </Select>
-                <Select 
-                    isDisabled
-                    label="How does viralyst work?" 
-                    placeholder="Select an animal"
-                    defaultSelectedKeys={["cat"]}
-                    className="max-w-xs"
-                >
-                    <SelectItem key="first question" value="this">
-                        AAAAA
-                    </SelectItem>
-                </Select>
-                    <div></div>
-                    <div></div>
-                    <div>How does viralyst work?</div>
-                    <div>Is viralyst available worldwide?</div>
-                    <div>How do i get started?</div>
+                <div className="border-2 border-red-500 pt-4 pl-4 mx-36 flex flex-col"> 
+                    <Accordion>
+                        <AccordionItem key="1" aria-label="What is Viralyst?"  title="What is Viralyst?">
+                            {defaultContent}
+                        </AccordionItem>
+                        <AccordionItem key="2" aria-label="How does viralyst work?" title="How does viralyst work?">
+                            {defaultContent}
+                        </AccordionItem>
+                        <AccordionItem key="3" aria-label="Is viralyst available worldwide?" title="Is viralyst available worldwide?">
+                            {defaultContent}
+                        </AccordionItem>
+                        <AccordionItem key="4" aria-label="How do i get started?" title="How do i get started?">
+                            {defaultContent}
+                        </AccordionItem>
+                    </Accordion>                    
                 </div>
             </div>
         </div>
