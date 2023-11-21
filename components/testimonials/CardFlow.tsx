fbc8ebf40b71b0fddb63db3553b300b7ae230604
Jay <140168334+Qdev1226@users.noreply.github.com>
2023-11-21 17:24:24 +0200

    complete slide

--- components/testimonials/CardFlow.tsx
+++ components/testimonials/CardFlow.tsx
@@ -1,4 +1,6 @@
+"use client"
 import {Card, CardHeader, CardBody, CardFooter, Avatar} from "@nextui-org/react";
+import {useState} from 'react'
 interface carditem {
     name : string,
     contact : string,
@@ -10,14 +12,19 @@ interface Props {
 }
 
 export const CardFlow : React.FC < Props > = ({data}) => {
+    const [active, setActive] = useState(0);
+    const handleClick = (i: number) => () => {
+        console.log(i)
+        setActive(i);
+    };
     return (
         <div className="max-w-[1280px] w-fit h-fit mx-auto">
             <div className="flex justify-between mb-[72px]">
-                <div
+                <div 
                     className="w-[716px] text-white text-5xl font-normal font-['Inter'] leading-[72px]">An enterprise to ramp up your company
                 </div>
                 <div className="flex mt-[70px]">
-                    <div
+                    <div onClick={handleClick(0)}
                         className="bg-white mr-7 hover:bg-gray-200 w-[72px] h-[72px] rounded-full flex justify-center items-center text-white">
                         <svg
                             width="36"
@@ -29,18 +36,18 @@ export const CardFlow : React.FC < Props > = ({data}) => {
                                 d="M28.5 18L7.5 18"
                                 stroke="#0A2640"
                                 strokeWidth="3"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"/>
+                                strokeLinecap="round"
+                                strokeLinejoin="round"/>
                             <path
                                 d="M18 28.5L7.5 18L18 7.5"
                                 stroke="#0A2640"
                                 strokeWidth="3"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"/>
+                                strokeLinecap="round"
+                                strokeLinejoin="round"/>
                         </svg>
 
                     </div>
-                    <div
+                    <div  onClick={handleClick(1)}
                         className="bg-white hover:bg-gray-200 w-[72px] h-[72px] rounded-full flex justify-center items-center text-white">
                         <svg
                             width="36"
@@ -52,21 +59,21 @@ export const CardFlow : React.FC < Props > = ({data}) => {
                                 d="M7.5 21H28.5"
                                 stroke="#0A2640"
                                 strokeWidth="3"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"/>
+                                strokeLinecap="round"
+                                strokeLinejoin="round"/>
                             <path
                                 d="M18 10.5L28.5 21L18 31.5"
                                 stroke="#0A2640"
                                 strokeWidth="3"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"/>
+                                strokeLinecap="round"
+                                strokeLinejoin="round"/>
                         </svg>
                     </div>
 
                 </div>
             </div>
-            <div className="flex ">
-                {data.map((item, index) => (
+            <div className="flex transition-all ">
+                {data.slice(active*3,active*3+3).map((item, index) => (
                     <Card className="max-w-[340px]  p-8 mx-4" key={index}>
                         <CardBody className="px-3 py-0 text-xl text-gray-900">
                             <p>
--- components/testimonials/index.tsx
+++ components/testimonials/index.tsx
@@ -17,6 +17,30 @@ const data: datatype[] = [
         {
             name: "Albus Dumbledore",
             contact:"Manager @ Howarts",
+            description: "Learning curve infrastructure value proposition advisor strategy user experience hypotheses investor",
+            avatar: ""
+        },
+        {
+            name: "Albus Dumbledore",
+            contact:"Manager @ Howarts",
+            description: "Release facebook responsive web design business model canvas seed money monetization.",
+            avatar: ""
+        },
+        {
+            name: "Albus Dumbledore",
+            contact:"Manager @ Howarts",
+            description: "Buyer buzz partner network disruptive non-disclosure agreement business",
+            avatar: ""
+        },
+        {
+            name: "Albus Dumbledore",
+            contact:"Manager @ Howarts",
+            description: "Buyer buzz partner network disruptive non-disclosure agreement business",
+            avatar: ""
+        },
+        {
+            name: "Albus Dumbledore",
+            contact:"Manager @ Howarts",
             description: "Buyer buzz partner network disruptive non-disclosure agreement business",
             avatar: ""
         },
