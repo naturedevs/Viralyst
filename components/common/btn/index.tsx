785099067483f15ee7eb8a86ae2789bc1e08102a
Jay <140168334+Qdev1226@users.noreply.github.com>
2023-11-21 09:53:21 +0200

    change node 18.2.0

--- components/common/btn/index.tsx
+++ components/common/btn/index.tsx
@@ -1,6 +1,6 @@
-export default function Btn() {
+export default function Btn({}) {
     return (
-        <div className="p-4 bg-white rounded text-black w-fit">
+        <div className="p-4 bg-[#D05404] text-lg font-bold px-8 py-4 rounded-full text-white w-fit">
             btn
         </div>
     )
